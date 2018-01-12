c1c6e65064e07d9a9320af468302d4d8172c6dab
Josh Dales <dales.josh@gmail.com>
2018-01-12 08:39:12 -0500

    Add some shadows to clock hands and fix some formatting

--- src/components/OpenTimes.js
+++ src/components/OpenTimes.js
@@ -4,12 +4,12 @@ class OpenTimes extends React.Component {
   displayTime(time) {
     const hour = parseInt(time.getHours(), 0);
     const min = parseInt(time.getMinutes(), 0);
-    const zeroMin = (min < 10 ? '0' : '') + min
+    const zeroMin = (min < 10 ? '0' : '') + min;
     if (hour < 13) {
       return `${hour}:${zeroMin}AM`;
     } else {
       return `${hour - 12}:${zeroMin}PM`;
-    };
+    }
   };
 
   render() {
@@ -20,7 +20,7 @@ class OpenTimes extends React.Component {
     return (
       <div className="times">
         <p className="todays-hours opening_time">
-          {is_open ? 'Open' : 'Opening'} at: <time>{this.displayTime(open_at)}</time>
+          {is_open ? 'Opened' : 'Open'} at: <time>{this.displayTime(open_at)}</time>
         </p>
         <p className="todays-hours closing_time">
           {is_open ? 'Closing' : 'Closed'} at: <time>{this.displayTime(close_at)}</time>
