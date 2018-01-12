8efbdff55246e3ec07b810ae7df03b7f988a35aa
Josh Dales <dales.josh@gmail.com>
2018-01-11 19:38:46 -0500

    Render hour clock hand

--- src/components/SystemTime.js
+++ src/components/SystemTime.js
@@ -4,6 +4,7 @@ class SystemTime extends React.Component {
   constructor(){
     super();
     this.renderMinHand = this.renderMinHand.bind(this);
+    this.renderHourHand = this.renderHourHand.bind(this);
   };
 
   renderMinHand() {
@@ -11,13 +12,22 @@ class SystemTime extends React.Component {
     const minDeg = ((min / 60) * 360) + 90;
     const divStyle = {transform: `rotate(${minDeg}deg)`};
 
-    return <div className="hand hour-hand" style={divStyle}></div>;
+    return <div className="hand min-hand" style={divStyle}></div>;
   };
+
+  renderHourHand() {
+    const hour = this.props.time.getHours();
+    const hourDeg = ((hour / 12) * 360) + 90;
+    const divStyle = {transform: `rotate(${hourDeg}deg)`};
+
+    return <div className="hand min-hand" style={divStyle}></div>;
+  }
 
   render() {
     return (
       <div className="clock">
           {this.renderMinHand()}
+          {this.renderHourHand()}
       </div>
     );
   };
