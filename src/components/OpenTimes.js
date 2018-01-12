b4dc4555f7fcccb59bda48d9c5f20400c0cf3800
Josh Dales <dales.josh@gmail.com>
2018-01-11 20:24:35 -0500

    Display opena dn close times in 12 hour format

--- src/components/OpenTimes.js
+++ src/components/OpenTimes.js
@@ -1,21 +1,31 @@
 import React from 'react';
 
 class OpenTimes extends React.Component {
+  displayTime(time) {
+    const timeNum = parseInt(time, 0);
+
+    if (timeNum < 13) {
+      return `${timeNum}AM`
+    } else {
+      return `${timeNum - 12}PM`
+    }
+  }
+
   render() {
-    const open_at = new Date(this.props.openHours.open_at)
-    const close_at = new Date(this.props.openHours.close_at)
+    const open_at = new Date(this.props.openHours.open_at);
+    const close_at = new Date(this.props.openHours.close_at);
 
     return (
       <div className="times">
         <p className="todays-hours opening_time">
-          Open at: <time>{open_at.getHours()}</time>
+          Open at: <time>{this.displayTime(open_at.getHours())}</time>
         </p>
         <p className="todays-hours closing_time">
-          Close at: <time>{close_at.getHours()}</time>
+          Close at: <time>{this.displayTime(close_at.getHours())}</time>
         </p>
       </div>
-    )
-  }
-}
+    );
+  };
+};
 
-export default OpenTimes
+export default OpenTimes;
